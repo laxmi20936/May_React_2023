9b3c2dac91bcb4e81a8483d759d190e57590c070
laxmi <waghmorelaxmi@gmail.com>
2023-05-19 13:22:48 +0530

    react episode 2

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-{/* <div id = "parent">
-    <div id = "child">
-        <h1>Practice practice</h1>
-    </div>
-</div> */}
-
-{/* <div id = "parent">
-    <div id = "child">
-        <h1>Practice practice</h1>
-        <h1>Practice practice 22</h1>
-    </div>
-</div> */}
-
-
-{/* <div id = "parent">
-    <div id = "child">
-        <h1>Practice practice</h1>
-        <h1>Practice practice 22</h1>
-    </div>
-    <div id = "child22">
-        <h1>Practice practice</h1>
-        <h1>Practice practice 22</h1>
-    </div>
-
-</div> */}
 
 const heading = React.createElement("div", {id : "parent"},  
                 [React.createElement("div",{id:"child"}, 
-                [React.createElement("h1",{},"Practice practice"), 
-                React.createElement("h1",{},"Practice practice 22")]),
+                [React.createElement("h1",{}," practice"), 
+                React.createElement("h1",{},"Practiceo ok  practice 22")]),
                 React.createElement("div",{id:"child22"}, 
-                [React.createElement("h1",{},"Practice practice"), 
+                [React.createElement("h1",{},"Practice N  practice"), 
                 React.createElement("h1",{},"Practice practice 22")])
                 ]);
 console.log(heading);
