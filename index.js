39f2f3137e49efba195cc4d5e9e1f8203289c15f
laxmi <waghmorelaxmi@gmail.com>
2023-05-22 17:15:28 +0530

    react-epi-4

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Funk = ()=> (
+    <div>
+        <h1>ran</h1>
+        <h1>nath</h1>
+    </div>
+)
+const heading = (<div>
+            <h1>Lax waghmore mi</h1>
+            <p>ppp qq</p>
+            <Funk/>
+   </div>)
 
-const heading = React.createElement("div", {id : "parent"},  
-                [React.createElement("div",{id:"child"}, 
-                [React.createElement("h1",{}," practice"), 
-                React.createElement("h1",{},"Practiceo ok  practice 22")]),
-                React.createElement("div",{id:"child22"}, 
-                [React.createElement("h1",{},"Practice N  practice"), 
-                React.createElement("h1",{},"Practice practice 22")])
-                ]);
+ var abc = "Coool";
+
+const Comp = () =>  <>
+        <>
+        <h1>oklpppp</h1>
+        <p>kk lax</p>
+        {heading}
+        <Funk/>
+        {2+9999} ,{abc}
+        <><h1>nhi</h1></>
+    </>
+    </>
+    
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Comp/>);
